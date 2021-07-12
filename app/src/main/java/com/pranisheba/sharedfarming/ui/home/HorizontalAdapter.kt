c6c0473cc4bcc3a48e32ee4eda1a2346dbc840ad
Monir <mknishad1@ymail.com>
2021-07-12 17:49:49 +0600

    add fund opportunities recycler view

---
+++ app/src/main/java/com/pranisheba/sharedfarming/ui/home/HorizontalAdapter.kt
@@ -0,0 +1,41 @@
+package com.pranisheba.sharedfarming.ui.home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.pranisheba.sharedfarming.R
+import com.squareup.picasso.Picasso
+
+
+class HorizontalAdapter(private val list: ArrayList<String>) :
+  RecyclerView.Adapter<HorizontalAdapter.MyView>() {
+
+  class MyView(view: View) : RecyclerView.ViewHolder(view) {
+    var imageView: ImageView = view.findViewById(R.id.image)
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyView {
+    val itemView: View = LayoutInflater
+      .from(parent.context)
+      .inflate(
+        R.layout.recycler_item,
+        parent,
+        false
+      )
+    return MyView(itemView)
+  }
+
+  override fun onBindViewHolder(holder: MyView, position: Int) {
+    val listData = list[position]
+
+    //Loading Image into view
+    Picasso.get().load(listData).placeholder(R.mipmap.ic_launcher).into(holder.imageView)
+
+  }
+
+  override fun getItemCount(): Int {
+    return list.size
+  }
+}

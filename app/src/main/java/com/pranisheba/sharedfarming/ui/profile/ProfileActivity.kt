37265453edb5228ebd0cfc332f488f15ff16531d
Monir <mknishad1@ymail.com>
2021-08-02 16:07:53 +0600

    add profile activity

---
+++ app/src/main/java/com/pranisheba/sharedfarming/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.pranisheba.sharedfarming.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pranisheba.sharedfarming.R
+
+class ProfileActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_profile)
+  }
+}
